2bb53b3ae18e6f56f4c89230f2450ed174fc7f61
pappuparvez1112 <pappuparvezshazz@gmail.com>
2023-06-04 03:00:09 +0600

    [Done] create-user-service

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
+
 const app: Application = express()
 
 app.use(cors())
@@ -8,9 +10,17 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+//Application routes
+app.use('/api/v1/users/', usersRouter)
+
 //FOR TESTING ROUTE
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+app.get('/', async (req: Request, res: Response) => {
+  // await userService.createUser({
+  //   id: '999',
+  //   password: '234567',
+  //   role: 'student',
+  // })
+  res.send('working successfully')
 })
 
 export default app
