74108feac4692a19f942d11597df3a427f2bdab9
pappuparvez1112 <pappuparvezshazz@gmail.com>
2023-06-08 03:09:42 +0600

    [Done] errorHandling setup globalErrorHandling unhandleRejection uncoughtException SIGTERM

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application } from 'express'
 import cors from 'cors'
-import usersRouter from './app/modules/users/users.route'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import { UserRoutes } from './app/modules/users/user.route'
+// import ApiError from './errors/ApiError'
 
 const app: Application = express()
 
@@ -11,17 +13,26 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 //Application routes
-console.log(app.get('env'))
-app.use('/api/v1/users/', usersRouter)
 
-//FOR TESTING ROUTE
-app.get('/', async (req: Request, res: Response) => {
-  // await userService.createUser({
-  //   id: '999',
-  //   password: '234567',
-  //   role: 'student',
-  // })
-  res.send('working successfully')
-})
+app.use('/api/v1/users/', UserRoutes)
+
+// //FOR TESTING ROUTE
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   throw new Error('testing error logger')
+//   //Promise.reject(new Error('Unhandle promise rejection'))
+//   //grace fully handling server and doing it server.ts to off
+//   // await userService.createUser({
+//   //   id: '999',
+//   //   password: '234567',
+//   //   role: 'student',
+//   // })
+//   // res.send('working successfully')
+
+//   //   throw new ApiError(400, 'generic error')
+//   //   next('next error')
+//   //   throw new Error('there was an error')
+// })
+
+app.use(globalErrorHandler)
 
 export default app
