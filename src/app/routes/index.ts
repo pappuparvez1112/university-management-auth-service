95e2d3698053eda8c62afbe21ab4ebc25a17c3dc
pappuparvez1112 <pappuparvezshazz@gmail.com>
2023-06-17 18:37:37 +0600

    [Done] setup complete academicfaculty academicdepartment crud operation populate() referencing id

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 import { SemesterRoutes } from '../modules/academicSemester/academicSemester.route';
 import { UserRoutes } from '../modules/user/user.route';
 
@@ -11,6 +13,14 @@ const moduleRoutes = [
   {
     path: '/academic-semester',
     route: SemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: AcademicDepartmentRoutes,
   },
 ];
 moduleRoutes.forEach(route => router.use(route.path, route.route));
